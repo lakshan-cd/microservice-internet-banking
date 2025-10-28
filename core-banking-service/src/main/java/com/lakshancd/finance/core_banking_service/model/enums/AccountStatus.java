6869478cc0b68574f0661b8db2917adb5153dd10
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-10-29 00:54:07 +0530

    add account status and type enums for core banking service

---
+++ core-banking-service/src/main/java/com/lakshancd/finance/core_banking_service/model/enums/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.lakshancd.finance.core_banking_service.model.enums;
+
+public enum AccountStatus {
+    PENDING,ACTIVE,DORMANT,BLOCKED
+}
---
+++ core-banking-service/src/main/java/com/lakshancd/finance/core_banking_service/model/enums/AccountType.java
@@ -0,0 +1,5 @@
+package com.lakshancd.finance.core_banking_service.model.enums;
+
+public enum AccountType {
+    SAVINGS_ACCOUNT, FIXED_DEPOSIT, LOAN_ACCOUNT
+}
