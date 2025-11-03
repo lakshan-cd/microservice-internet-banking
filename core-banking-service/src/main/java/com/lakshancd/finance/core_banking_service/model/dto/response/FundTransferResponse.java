bfd0c37989a353a99111d3f2fac8002e941d7035
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-11-03 22:10:58 +0530

    add DTOs for fund transfer and utility payment requests and responses

---
+++ core-banking-service/src/main/java/com/lakshancd/finance/core_banking_service/model/dto/response/FundTransferResponse.java
@@ -0,0 +1,13 @@
+package com.lakshancd.finance.core_banking_service.model.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class FundTransferResponse {
+    private String message;
+    private String transactionId;
+}
