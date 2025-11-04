bac38145109c800ad71a834745d401b5bccff512
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-11-04 23:19:41 +0530

    add BankingCoreRestClient for user data retrieval and enable Feign clients

---
+++ internet-banking-user-service/src/main/java/com/lakshancd/finance/internet_banking_user_service/service/feignclinet/BankingCoreRestClient.java
@@ -0,0 +1,14 @@
+package com.lakshancd.finance.internet_banking_user_service.service.feignclinet;
+
+import com.lakshancd.finance.internet_banking_user_service.model.dto.user.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "internet-banking-core-banking-service")
+public interface BankingCoreRestClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/api/v1/user/{identification}")
+    UserResponse readUser(@PathVariable("identification") String identification);
+
+}
