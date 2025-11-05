3237725894a5a62ecbcd5e936a4c97b880f38285
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-11-05 14:15:32 +0530

    add CustomFeignClientConfiguration and CustomFeignErrorDecoder for enhanced error handling in Feign clients

---
+++ internet-banking-user-service/src/main/java/com/lakshancd/finance/internet_banking_user_service/configuration/CustomFeignErrorDecoder.java
@@ -0,0 +1,64 @@
+package com.lakshancd.finance.internet_banking_user_service.configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lakshancd.finance.internet_banking_user_service.exception.SimpleBankingGlobalException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class CustomFeignErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String s, Response response) {
+        SimpleBankingGlobalException simpleBankingGlobalException = extractBankingCoreGlobalException(response);
+
+        return switch (response.status()) {
+            case 400 -> {
+                log.error("Error in request went through feign client {} ", simpleBankingGlobalException.getMessage() + " - " + simpleBankingGlobalException.getCode());
+                yield simpleBankingGlobalException;
+            }
+            case 401 -> {
+                log.error("Unauthorized Request Through Feign");
+                yield new Exception("Unauthorized Request Through Feign");
+            }
+            case 404 -> {
+                log.error("Unidentified Request Through Feign ");
+                yield new Exception("Unidentified Request Through Feign");
+            }
+            default -> {
+                log.error("Error in request went through feign client");
+                yield new Exception("Common Feign Exception");
+            }
+        };
+    }
+
+    private SimpleBankingGlobalException extractBankingCoreGlobalException(Response response) {
+        SimpleBankingGlobalException exceptionMessage = null;
+        Reader reader = null;
+        //capture the response body and convert it to SimpleBankingGlobalException
+        try{
+            reader = response.body().asReader(StandardCharsets.UTF_8);
+            String result = IOUtils.toString(reader);
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            exceptionMessage = objectMapper.readValue(result , SimpleBankingGlobalException.class);
+        } catch (IOException e){
+            log.error("IO Exception on reading exception message feign client" + e);
+        }finally {
+            try {
+                if (reader != null){
+                    reader.close();
+                }
+            } catch (IOException e){
+                log.error("IO Exception on reading exception message feign client" + e);
+            }
+        }
+        return exceptionMessage;
+    }
+}
