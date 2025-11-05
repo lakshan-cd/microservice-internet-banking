6f2e6b2ef3f459d0089f3808a3694b07ca07eb43
Lakshan Chamara <87594213+lakshan-cd@users.noreply.github.com>
2025-11-05 14:16:19 +0530

    add ErrorResponse class for structured error response handling

---
+++ internet-banking-user-service/src/main/java/com/lakshancd/finance/internet_banking_user_service/exception/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.lakshancd.finance.internet_banking_user_service.exception.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ErrorResponse {
+    private String code;
+    private String message;
+}
